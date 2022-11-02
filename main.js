dc60343496cf820f6179ab1bc3775f9b70387bf8
Amal Babu <amalb0220@gmail.com>
2022-11-02 12:00:25 +0530

    setted items

--- main.js
+++
@@ -1,9 +0,0 @@
-let nav=document.querySelector(".navbar")
-window.onscroll=function(){
-    if(document.documentElement.scrollTop>50){
-
-        nav.classList.add("header-scrolled");
-    }else {
-        nav.classList.remove("header-scrolled");
-    }
-}
